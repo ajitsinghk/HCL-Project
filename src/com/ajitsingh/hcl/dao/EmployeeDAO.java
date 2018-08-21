e6561a7e2fac30edd1411e9693dbf1f7e29809aa
Ajitsingh <ak01704n@pace.edu>
2018-08-20 23:43:22 -0400

    dao implementation

---
+++ src/com/ajitsingh/hcl/dao/EmployeeDAO.java
@@ -0,0 +1,11 @@
+package com.ajitsingh.hcl.dao;
+
+import java.util.List;
+
+import com.ajitsingh.hcl.entity.Employee;
+
+public interface EmployeeDAO {
+	
+	public List<Employee> getEmployee();
+
+}
---
+++ src/com/ajitsingh/hcl/dao/EmployeeDAOImpl.java
@@ -0,0 +1,41 @@
+package com.ajitsingh.hcl.dao;
+
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import com.ajitsingh.hcl.entity.Employee;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO{
+
+	//Injecting session factory
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	
+	//Spring Transactional Management
+	@Override
+	@Transactional
+	public List<Employee> getEmployee() {
+		
+		// Open hb session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// write query
+		Query<Employee> myQuery = currentSession.createQuery("from Employee", Employee.class);
+		
+		// executing query and storing in variable
+		List<Employee> employee = myQuery.getResultList();
+		
+		// return result
+		return employee;
+	}
+	
+	
+	
+
+}
