4882fea59bf9c7ef7471bcdd4e060c808c7924ae
Ajitsingh <ak01704n@pace.edu>
2018-08-21 09:25:37 -0400

    dao-injection
    dao ijected and rendered to view

--- WebContent/WEB-INF/view/list-employees.jsp
+++ WebContent/WEB-INF/view/list-employees.jsp
@@ -1,3 +1,5 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
 <!DOCTYPE html>
 <html>
 
@@ -7,8 +9,31 @@
 <title>List Employees</title>
 </head>
 <body>
+	<div id="wrapper">
+		<div id="header">
+			<h2>HCL Employee</h2>
+		</div>
+		<div id="container">
+			<div id="content">
+				<table>
+					<tr>
+						<th>First Name</th>
+						<th>Last Name</th>
+						<th>Email Name</th>
+					</tr>
+					<!-- rendering data from controller --> 
+					<c:forEach var="tempEmployee" items="${employees}">
+						<tr>
+							<td>${tempEmployee.firstName}</td>
+							<td>${tempEmployee.lastName}</td>
+							<td>${tempEmployee.email}</td>
+						</tr>
+					</c:forEach>
+				</table>
+			</div>
+		</div>
+	</div>
 
-List of employees
 
 </body>
 
--- src/com/ajitsingh/hcl/controller/EmployeeController.java
+++ src/com/ajitsingh/hcl/controller/EmployeeController.java
@@ -1,17 +1,35 @@
 package com.ajitsingh.hcl.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ajitsingh.hcl.dao.EmployeeDAO;
+import com.ajitsingh.hcl.entity.Employee;
 
 @Controller
 @RequestMapping("employee")
 public class EmployeeController {
 	
+	//INjecting DAO in controller
+	@Autowired
+	private EmployeeDAO employeeDAO;
+	
+	
 	
 	@RequestMapping("list")
 	public String listEmployees(Model theModel) {
 		
+		// getting employee from dao
+		List<Employee> theEmployee   = employeeDAO.getEmployee();
+
+		// adding employee to model and passing this variable rendering
+		theModel.addAttribute("employees", theEmployee);
+		
+		// return view page
 		return "list-employees";
 	}
 
